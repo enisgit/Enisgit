d51e0d602284ffd0a2155ffb3cb9f10cbadfd12e
Enida Sadiku <enidasadiku@gmail.com>
2024-04-03 12:53:57 +0100

    First hello world

--- Python2.py
+++ Python2.py
@@ -1,2 +1,21 @@
 print("Hello World")
-       
+import os
+
+# Create three new folders
+os.makedirs("folder1", exist_ok=True)
+os.makedirs("folder2", exist_ok=True)
+os.makedirs("folder3", exist_ok=True)
+
+# Navigate inside folder1 and create three new folders
+os.makedirs("folder1/subfolder1", exist_ok=True)
+os.makedirs("folder1/subfolder2", exist_ok=True)
+os.makedirs("folder1/subfolder3", exist_ok=True)
+
+# Remove two folders from folder1
+try:
+    os.rmdir("folder1/subfolder2")
+    os.rmdir("folder1/subfolder3")
+except FileNotFoundError:
+    pass
+
+
